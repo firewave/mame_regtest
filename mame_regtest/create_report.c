44f587eade026d5d30d1cc2ee94a29fa85070cd2
Administrator <Administrator@17dd1c5f-0563-3241-acae-1f7b577065e3>
2008-10-21 16:49:51 +0000

    create_report now has a XML config

--- mame_regtest/create_report.c
+++ mame_regtest/create_report.c
@@ -7,6 +7,7 @@
 
 /* local */
 #include "common.h"
+#include "config.h"
 
 /*
 	TODO:
@@ -131,7 +132,29 @@ static void free_group_data(struct group_data* gd)
 	};
 }
 
-struct report_1_cb_data
+static char* config_xml_folder = NULL;
+static char* config_output_file = NULL;
+static int config_print_stderr = 0;
+static int config_dokuwiki_format = 0;
+static int config_ignore_exitcode_4 = 0;
+static int config_show_memleaks = 0;
+static int config_show_clipped = 0;
+static int config_group_data = 0;
+
+struct config_entry report_config[] =
+{
+	{ "xml_folder", 		CFG_STR_PTR, 	&config_xml_folder },
+	{ "output_file", 		CFG_STR_PTR, 	&config_output_file },
+	{ "print_stderr", 		CFG_INT, 		&config_print_stderr },
+	{ "dokuwiki_format", 	CFG_INT, 		&config_dokuwiki_format },
+	{ "ignore_exitcode_4", 	CFG_INT, 		&config_ignore_exitcode_4 },
+	{ "show_memleaks", 		CFG_INT, 		&config_show_memleaks },
+	{ "show_clipped", 		CFG_INT, 		&config_show_clipped },
+	{ "group_data", 		CFG_INT, 		&config_group_data },
+	{ NULL, 				-1, 			NULL }
+};
+
+struct report_cb_data
 {
 	FILE* report_fd;
 	int print_stderr;
@@ -141,7 +164,7 @@ struct report_1_cb_data
 	int show_clipped;
 };
 
-static int create_report_from_filename(const char *const filename, const struct report_1_cb_data *const r1_cb_data, int write_src_header)
+static int create_report_from_filename(const char *const filename, const struct report_cb_data *const r_cb_data, int write_src_header)
 {
 	int data_written = 0;
 
@@ -163,73 +186,73 @@ static int create_report_from_filename(const char *const filename, const struct 
 					xmlChar* exitcode_key = xmlGetProp(output_childs, (const xmlChar*)"exitcode");
 
 					xmlChar* stderr_key = NULL;
-					if( r1_cb_data->print_stderr )
+					if( r_cb_data->print_stderr )
 						stderr_key = xmlGetProp(output_childs, (const xmlChar*)"stderr");
 					xmlChar* stdout_key = NULL;
-					if( r1_cb_data->show_clipped )
+					if( r_cb_data->show_clipped )
 						stdout_key = xmlGetProp(output_childs, (const xmlChar*)"stdout");
 
 					if( (exitcode_key && (xmlStrcmp(exitcode_key, (const xmlChar*)"0") != 0) && 
-						!(r1_cb_data->ignore_exitcode_4 && (xmlStrcmp(exitcode_key, (const xmlChar*)"4") == 0))) || 
-						(stderr_key && xmlStrstr(stderr_key, (const xmlChar*)"--- memory leak warning ---") && r1_cb_data->show_memleaks) ||
-						(r1_cb_data->show_clipped && stdout_key && xmlStrstr(stdout_key, (const xmlChar*)"clipped") && xmlStrstr(stdout_key, (const xmlChar*)" 0% samples clipped") == NULL) ) {
+						!(r_cb_data->ignore_exitcode_4 && (xmlStrcmp(exitcode_key, (const xmlChar*)"4") == 0))) || 
+						(stderr_key && xmlStrstr(stderr_key, (const xmlChar*)"--- memory leak warning ---") && r_cb_data->show_memleaks) ||
+						(r_cb_data->show_clipped && stdout_key && xmlStrstr(stdout_key, (const xmlChar*)"clipped") && xmlStrstr(stdout_key, (const xmlChar*)" 0% samples clipped") == NULL) ) {
 	
-						if( r1_cb_data->dokuwiki_format )
+						if( r_cb_data->dokuwiki_format )
 						{
 							if( write_src_header ) {
-								fprintf(r1_cb_data->report_fd, "===== %s =====\n", sourcefile_key);
+								fprintf(r_cb_data->report_fd, "===== %s =====\n", sourcefile_key);
 								/* disable so the header is only written once */
 								write_src_header = 0;
 							}
-							fprintf(r1_cb_data->report_fd, "==== %s ====\n", name_key);
+							fprintf(r_cb_data->report_fd, "==== %s ====\n", name_key);
 							if( bios_key )
-								fprintf(r1_cb_data->report_fd, "  * BIOS: ''%s''\n", bios_key);
+								fprintf(r_cb_data->report_fd, "  * BIOS: ''%s''\n", bios_key);
 							if( ramsize_key )
-								fprintf(r1_cb_data->report_fd, "  * Ramsize: ''%s''\n", ramsize_key);
+								fprintf(r_cb_data->report_fd, "  * Ramsize: ''%s''\n", ramsize_key);
 							if( devices_node )
 							{
 								xmlAttrPtr dev_attrs = devices_node->properties;
 								while( dev_attrs ) {
-									fprintf(r1_cb_data->report_fd, "  * Device: ''%s %s''\n", dev_attrs->name, dev_attrs->children->content);
+									fprintf(r_cb_data->report_fd, "  * Device: ''%s %s''\n", dev_attrs->name, dev_attrs->children->content);
 	
 									dev_attrs = dev_attrs->next;
 								}
 							}
 							if( autosave_key )
-								fprintf(r1_cb_data->report_fd, " (autosave)");
-							fprintf(r1_cb_data->report_fd, "  * Error code: ''%s''\n", exitcode_key);
+								fprintf(r_cb_data->report_fd, " (autosave)");
+							fprintf(r_cb_data->report_fd, "  * Error code: ''%s''\n", exitcode_key);
 
 							if( stdout_key )
-								fprintf(r1_cb_data->report_fd, "<code>\n%s\n</code>\n", stdout_key);
+								fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n", stdout_key);
 	
 							if( stderr_key )
-								fprintf(r1_cb_data->report_fd, "<code>\n%s\n</code>\n\n", stderr_key); // TODO: why two '\n'
+								fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n\n", stderr_key); // TODO: why two '\n'
 						}
 						else
 						{
-							fprintf(r1_cb_data->report_fd, "%s: %s", sourcefile_key, name_key);
+							fprintf(r_cb_data->report_fd, "%s: %s", sourcefile_key, name_key);
 							if( bios_key )
-								fprintf(r1_cb_data->report_fd, " (bios %s)", bios_key);
+								fprintf(r_cb_data->report_fd, " (bios %s)", bios_key);
 							if( ramsize_key )
-								fprintf(r1_cb_data->report_fd, " (ramsize %s)", ramsize_key);
+								fprintf(r_cb_data->report_fd, " (ramsize %s)", ramsize_key);
 							if( devices_node )
 							{
 								xmlAttrPtr dev_attrs = devices_node->properties;
 								while( dev_attrs ) {
-									fprintf(r1_cb_data->report_fd, " (%s %s)", dev_attrs->name, dev_attrs->children->content);
+									fprintf(r_cb_data->report_fd, " (%s %s)", dev_attrs->name, dev_attrs->children->content);
 	
 									dev_attrs = dev_attrs->next;
 								}
 							}
 							if( autosave_key )
-								fprintf(r1_cb_data->report_fd, " (autosave)");
-							fprintf(r1_cb_data->report_fd, " (%s)\n", exitcode_key);
+								fprintf(r_cb_data->report_fd, " (autosave)");
+							fprintf(r_cb_data->report_fd, " (%s)\n", exitcode_key);
 
 							if( stdout_key )
-								fprintf(r1_cb_data->report_fd, "%s\n", stdout_key);
+								fprintf(r_cb_data->report_fd, "%s\n", stdout_key);
 	
 							if( stderr_key )
-								fprintf(r1_cb_data->report_fd, "%s\n", stderr_key);
+								fprintf(r_cb_data->report_fd, "%s\n", stderr_key);
 						}
 	
 						data_written = 1;
@@ -265,7 +288,7 @@ static int create_report_from_filename(const char *const filename, const struct 
 	return data_written;
 }
 
-static void create_report_from_group_data(struct group_data* gd, struct report_1_cb_data* r1_cb_data)
+static void create_report_from_group_data(struct group_data* gd, struct report_cb_data* r_cb_data)
 {
 	struct group_data* gd_entry = gd;
 	while( gd_entry ) {
@@ -273,7 +296,7 @@ static void create_report_from_group_data(struct group_data* gd, struct report_1
 		
 		struct driver_data* dd_entry = gd_entry->drivers;
 		while( dd_entry ) {
-			int data_written = create_report_from_filename(dd_entry->filename, r1_cb_data, write_src_header);
+			int data_written = create_report_from_filename(dd_entry->filename, r_cb_data, write_src_header);
 			if( data_written )
 				write_src_header = 0;
 			dd_entry = dd_entry->next;
@@ -282,296 +305,60 @@ static void create_report_from_group_data(struct group_data* gd, struct report_1
 	};
 }
 
-static void create_report_1_cb(struct parse_callback_data* pcd)
+static void create_report_cb(struct parse_callback_data* pcd)
 {
 	if( pcd->type == ENTRY_FILE ) {		
 		if( strstr(pcd->entry_name, ".xml") && strstr(pcd->entry_name, "listxml.xml") == NULL )
 		{
-			struct report_1_cb_data* r1_cb_data = (struct report_1_cb_data*)pcd->user_data;
+			struct report_cb_data* r_cb_data = (struct report_cb_data*)pcd->user_data;
 
 			printf("%s\n", pcd->entry_name);
-			create_report_from_filename(pcd->fullname, r1_cb_data, 1);
+			create_report_from_filename(pcd->fullname, r_cb_data, 1);
 		}
 	}
 }
 
-static void create_report_1(const char* dirname)
+static void create_report()
 {
-	FILE* report_1_fd = fopen("d:\\report_1.txt", "wb");
+	FILE* report_fd = fopen(config_output_file, "wb");
 
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_1_fd;
-	r1_cb_data.print_stderr = 0;
-	r1_cb_data.dokuwiki_format = 0;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
+	struct report_cb_data r_cb_data;
+	r_cb_data.report_fd = report_fd;
+	r_cb_data.print_stderr = config_print_stderr;
+	r_cb_data.dokuwiki_format = config_dokuwiki_format;
+	r_cb_data.ignore_exitcode_4 = config_ignore_exitcode_4;
+	r_cb_data.show_memleaks = config_show_memleaks;
+	r_cb_data.show_clipped = config_show_clipped;
 
-	parse_directory(dirname, 0, create_report_1_cb, (void*)&r1_cb_data);
-
-	fclose(report_1_fd);
-	report_1_fd = NULL;
-}
-
-static void create_report_2(const char* dirname)
-{
-	FILE* report_2_fd = fopen("d:\\report_2.txt", "wb");
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_2_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 0;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	parse_directory(dirname, 0, create_report_1_cb, (void*)&r1_cb_data);
-
-	fclose(report_2_fd);
-	report_2_fd = NULL;
-}
-
-static void create_report_3(const char* dirname)
-{
-	FILE* report_3_fd = fopen("d:\\report_3.txt", "wb");
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_3_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 1;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	parse_directory(dirname, 0, create_report_1_cb, (void*)&r1_cb_data);
-
-	fclose(report_3_fd);
-	report_3_fd = NULL;
-}
-
-static void create_report_4(const char* dirname)
-{
-	FILE* report_4_fd = fopen("d:\\report_4.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_4_fd;
-	r1_cb_data.print_stderr = 0;
-	r1_cb_data.dokuwiki_format = 0;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_4_fd);
-	report_4_fd = NULL;
-}
-
-static void create_report_5(const char* dirname)
-{
-	FILE* report_5_fd = fopen("d:\\report_5.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_5_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format =  0;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_5_fd);
-	report_5_fd = NULL;
-}
-
-static void create_report_6(const char* dirname)
-{
-	FILE* report_6_fd = fopen("d:\\report_6.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_6_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 1;
-	r1_cb_data.ignore_exitcode_4 = 0;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
+	if( config_group_data ) {
+		struct group_data* gd = NULL;
+		get_group_data(config_xml_folder, &gd);
 	
-	free_group_data(gd);
+		printf("creating report...\n");
+		create_report_from_group_data(gd, &r_cb_data);
 
-	fclose(report_6_fd);
-	report_6_fd = NULL;
-}
+		free_group_data(gd);
+	}
+	else
+		parse_directory(config_xml_folder, 0, create_report_cb, (void*)&r_cb_data);
 
-static void create_report_7(const char* dirname)
-{
-	FILE* report_7_fd = fopen("d:\\report_7.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_7_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 1;
-	r1_cb_data.ignore_exitcode_4 = 1;
-	r1_cb_data.show_memleaks = 0;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_7_fd);
-	report_7_fd = NULL;
-}
-
-static void create_report_8(const char* dirname)
-{
-	FILE* report_8_fd = fopen("d:\\report_8.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_8_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 0;
-	r1_cb_data.ignore_exitcode_4 = 1;
-	r1_cb_data.show_memleaks = 1;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_8_fd);
-	report_8_fd = NULL;
-}
-
-static void create_report_9(const char* dirname)
-{
-	FILE* report_9_fd = fopen("d:\\report_9.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_9_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 1;
-	r1_cb_data.ignore_exitcode_4 = 1;
-	r1_cb_data.show_memleaks = 1;
-	r1_cb_data.show_clipped = 0;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_9_fd);
-	report_9_fd = NULL;
-}
-static void create_report_10(const char* dirname)
-{
-	FILE* report_10_fd = fopen("d:\\report_10.txt", "wb");
-
-	struct group_data* gd = NULL;
-	get_group_data(dirname, &gd);
-
-	struct report_1_cb_data r1_cb_data;
-	r1_cb_data.report_fd = report_10_fd;
-	r1_cb_data.print_stderr = 1;
-	r1_cb_data.dokuwiki_format = 1;
-	r1_cb_data.ignore_exitcode_4 = 1;
-	r1_cb_data.show_memleaks = 1;
-	r1_cb_data.show_clipped = 1;
-
-	printf("creating report...\n");
-	create_report_from_group_data(gd, &r1_cb_data);
-
-	free_group_data(gd);
-
-	fclose(report_10_fd);
-	report_10_fd = NULL;
+	fclose(report_fd);
+	report_fd = NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	if( argc != 3 ) {
-		printf("usage: create_report <report_number> <folder>\n");
-		printf("\n");
-		printf("reports:\n");
-		printf("\t 1 - list of drivers, that didn't return 0\n");
-		printf("\t 2 - list of drivers, that didn't return 0 (with stderr)\n");
-		printf("\t 3 - list of drivers, that didn't return 0 (with stderr) (DokuWiki format)\n");
-		printf("\t 4 - list of drivers, that didn't return 0 (grouped by srcfile)\n");
-		printf("\t 5 - list of drivers, that didn't return 0 (with stderr) (grouped by srcfile)\n");
-		printf("\t 6 - list of drivers, that didn't return 0 (with stderr) (DokuWiki format) (grouped by srcfile)\n");
-		printf("\t 7 - list of drivers, that didn't return 0 (with stderr) (DokuWiki format) (grouped by srcfile) (ignore exitcode 4)\n");
-		printf("\t 8 - list of drivers, that didn't return 0 (with stderr) (grouped by srcfile) (ignore exitcode 4) (show memory leaks)\n");
-		printf("\t 9 - list of drivers, that didn't return 0 (with stderr) (DokuWiki format) (grouped by srcfile) (ignore exitcode 4) (show memory leaks)\n");
-		printf("\t10 - list of drivers, that didn't return 0 (with stderr) (DokuWiki format) (grouped by srcfile) (ignore exitcode 4) (show memory leaks) (show clipped)\n");
+	if( argc != 2 ) {
+		printf("usage: create_report <report_name>\n");
 		exit(1);
 	}
 	
-	int report_number = atoi(argv[1]);
-	switch( report_number )
-	{
-		case 1:
-			create_report_1(argv[2]);
-			break;
-		case 2:
-			create_report_2(argv[2]);
-			break;
-		case 3:
-			create_report_3(argv[2]);
-			break;
-		case 4:
-			create_report_4(argv[2]);
-			break;
-		case 5:
-			create_report_5(argv[2]);
-			break;
-		case 6:
-			create_report_6(argv[2]);
-			break;
-		case 7:
-			create_report_7(argv[2]);
-			break;
-		case 8:
-			create_report_8(argv[2]);
-			break;
-		case 9:
-			create_report_9(argv[2]);
-			break;
-		case 10:
-			create_report_10(argv[2]);
-			break;
-		default:
-			printf("unknown report\n");
-			exit(1);
-	}
+	config_init("create_report.xml", "create_report");
+	config_read(report_config, argv[1]);
+	
+	create_report();
+	
+	config_free(report_config);
 	
 	return 0;
 }
