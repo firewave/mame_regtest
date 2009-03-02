3a92276915f7b195071ba7178798bd40e8f2d7a8
Administrator <Administrator@17dd1c5f-0563-3241-acae-1f7b577065e3>
2009-03-02 21:30:30 +0000

    - added option "report_type" (0 = result report / 1 = comparison report) - added option "compare_folder" (used for comparison report to compare "xml_folder" against) - implemented comparison report (checks "exitcode", "stderr" and "png_crc")

--- mame_regtest/create_report.c
+++ mame_regtest/create_report.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 /* libxml2 */
 #include "libxml/parser.h"
+#include "libxml/xpath.h"
 
 /* local */
 #include "common.h"
@@ -142,6 +144,8 @@ static int config_ignore_exitcode_4 = 0;
 static int config_show_memleaks = 0;
 static int config_show_clipped = 0;
 static int config_group_data = 0;
+static int config_report_type = 0; /* 0 = result report - 1 = comparison report */
+static char* config_compare_folder = NULL;
 
 struct config_entry report_config[] =
 {
@@ -153,6 +157,8 @@ struct config_entry report_config[] =
 	{ "show_memleaks", 		CFG_INT, 		&config_show_memleaks },
 	{ "show_clipped", 		CFG_INT, 		&config_show_clipped },
 	{ "group_data", 		CFG_INT, 		&config_group_data },
+	{ "report_type",		CFG_INT,		&config_report_type },
+	{ "compare_folder",		CFG_STR_PTR,	&config_compare_folder }, /* comparison report specific */
 	{ NULL, 				-1, 			NULL }
 };
 
@@ -164,127 +170,224 @@ struct report_cb_data
 	int ignore_exitcode_4;
 	int show_memleaks;
 	int show_clipped;
+	int report_type;
+	const char* compare_folder;
 };
+
+/* TODO - handle multiple occurances */
+static xmlChar* get_attribute_by_xpath(xmlXPathContextPtr xpathCtx, const xmlChar* xpath_expr, const xmlChar* attr_name)
+{
+	xmlChar* attr_value = NULL;
+
+	xmlXPathObjectPtr xpathObj = xmlXPathEvalExpression(xpath_expr, xpathCtx);
+	if( xpathObj && xpathObj->nodesetval && xpathObj->nodesetval->nodeNr > 0 ) {
+		attr_value = xmlGetProp(xpathObj->nodesetval->nodeTab[0], attr_name);
+	}
+	if( xpathObj )
+		xmlXPathFreeObject(xpathObj);
+
+	return attr_value;
+}
+
+/* TODO: implement dokuwiki format */
+#define COMPARE_ATTRIBUTE(xpath_expr, attr_name) \
+{ \
+	xmlChar* attr1 = get_attribute_by_xpath(xpathCtx1, (const xmlChar*)xpath_expr, (const xmlChar*)attr_name); \
+	xmlChar* attr2 = get_attribute_by_xpath(xpathCtx2, (const xmlChar*)xpath_expr, (const xmlChar*)attr_name); \
+	\
+	if( xmlStrcmp(attr1, attr2) != 0 ) \
+	{ \
+		if( write_set_data ) \
+		{ \
+			write_set_data = 0; \
+			xmlChar* name = get_attribute_by_xpath(xpathCtx1, (const xmlChar*)"/output", (const xmlChar*)"name"); \
+			xmlChar* srcfile = get_attribute_by_xpath(xpathCtx1, (const xmlChar*)"/output", (const xmlChar*)"sourcefile"); \
+			\
+			fprintf(r_cb_data->report_fd, "%s: %s\n", srcfile, name); \
+			\
+			xmlFree(name); \
+			name = NULL; \
+			xmlFree(srcfile); \
+			srcfile = NULL; \
+		} \
+		fprintf(r_cb_data->report_fd, "'%s' differs\n", attr_name);\
+		fprintf(r_cb_data->report_fd, "\told: %s\n", attr1); \
+		fprintf(r_cb_data->report_fd, "\tnew: %s\n", attr2); \
+	} \
+	\
+	xmlFree(attr2); \
+	attr2 = NULL; \
+	xmlFree(attr1); \
+	attr1 = NULL; \
+}
 
 static int create_report_from_filename(const char *const filename, const struct report_cb_data *const r_cb_data, int write_src_header)
 {
 	int data_written = 0;
-
-	xmlDocPtr doc = xmlReadFile(filename, "UTF-8", 0);
-	if( doc ) {
-		xmlNodePtr output_node = doc->children;
 	
-		xmlChar* name_key = xmlGetProp(output_node, (const xmlChar*)"name");
-		xmlChar* sourcefile_key = xmlGetProp(output_node, (const xmlChar*)"sourcefile");
-		xmlChar* bios_key = xmlGetProp(output_node, (const xmlChar*)"bios");
-		xmlChar* ramsize_key = xmlGetProp(output_node, (const xmlChar*)"ramsize");
-		xmlChar* autosave_key = xmlGetProp(output_node, (const xmlChar*)"autosave");
-	
-		xmlNodePtr output_childs = output_node->children;
-		xmlNodePtr devices_node = NULL;
-		while( output_childs ) {
-			if( output_childs->type == XML_ELEMENT_NODE ) {
-				if( xmlStrcmp(output_childs->name, (const xmlChar*)"result") == 0 ) {
-					xmlChar* exitcode_key = xmlGetProp(output_childs, (const xmlChar*)"exitcode");
-
-					xmlChar* stderr_key = NULL;
-					if( r_cb_data->print_stderr )
-						stderr_key = xmlGetProp(output_childs, (const xmlChar*)"stderr");
-					xmlChar* stdout_key = NULL;
-					if( r_cb_data->show_clipped )
-						stdout_key = xmlGetProp(output_childs, (const xmlChar*)"stdout");
-
-					if( (exitcode_key && (xmlStrcmp(exitcode_key, (const xmlChar*)"0") != 0) && 
-						!(r_cb_data->ignore_exitcode_4 && (xmlStrcmp(exitcode_key, (const xmlChar*)"4") == 0))) || 
-						(stderr_key && xmlStrstr(stderr_key, (const xmlChar*)"--- memory leak warning ---") && r_cb_data->show_memleaks) ||
-						(r_cb_data->show_clipped && stdout_key && xmlStrstr(stdout_key, (const xmlChar*)"clipped") && xmlStrstr(stdout_key, (const xmlChar*)" 0% samples clipped") == NULL) ) {
-	
-						if( r_cb_data->dokuwiki_format )
-						{
-							if( write_src_header ) {
-								fprintf(r_cb_data->report_fd, "===== %s =====\n", sourcefile_key);
-								/* disable so the header is only written once */
-								write_src_header = 0;
-							}
-							fprintf(r_cb_data->report_fd, "==== %s ====\n", name_key);
-							if( bios_key )
-								fprintf(r_cb_data->report_fd, "  * BIOS: ''%s''\n", bios_key);
-							if( ramsize_key )
-								fprintf(r_cb_data->report_fd, "  * Ramsize: ''%s''\n", ramsize_key);
-							if( devices_node )
-							{
-								xmlAttrPtr dev_attrs = devices_node->properties;
-								while( dev_attrs ) {
-									fprintf(r_cb_data->report_fd, "  * Device: ''%s %s''\n", dev_attrs->name, dev_attrs->children->content);
-	
-									dev_attrs = dev_attrs->next;
+	switch( r_cb_data->report_type)
+	{
+		default:
+		case 0:
+		{
+			xmlDocPtr doc = xmlReadFile(filename, "UTF-8", 0);
+			if( doc ) {
+				xmlNodePtr output_node = doc->children;
+			
+				xmlChar* name_key = xmlGetProp(output_node, (const xmlChar*)"name");
+				xmlChar* sourcefile_key = xmlGetProp(output_node, (const xmlChar*)"sourcefile");
+				xmlChar* bios_key = xmlGetProp(output_node, (const xmlChar*)"bios");
+				xmlChar* ramsize_key = xmlGetProp(output_node, (const xmlChar*)"ramsize");
+				xmlChar* autosave_key = xmlGetProp(output_node, (const xmlChar*)"autosave");
+			
+				xmlNodePtr output_childs = output_node->children;
+				xmlNodePtr devices_node = NULL;
+				while( output_childs ) {
+					if( output_childs->type == XML_ELEMENT_NODE ) {
+						if( xmlStrcmp(output_childs->name, (const xmlChar*)"result") == 0 ) {
+							xmlChar* exitcode_key = xmlGetProp(output_childs, (const xmlChar*)"exitcode");
+		
+							xmlChar* stderr_key = NULL;
+							if( r_cb_data->print_stderr )
+								stderr_key = xmlGetProp(output_childs, (const xmlChar*)"stderr");
+							xmlChar* stdout_key = NULL;
+							if( r_cb_data->show_clipped )
+								stdout_key = xmlGetProp(output_childs, (const xmlChar*)"stdout");
+		
+							if( (exitcode_key && (xmlStrcmp(exitcode_key, (const xmlChar*)"0") != 0) && 
+								!(r_cb_data->ignore_exitcode_4 && (xmlStrcmp(exitcode_key, (const xmlChar*)"4") == 0))) || 
+								(stderr_key && xmlStrstr(stderr_key, (const xmlChar*)"--- memory leak warning ---") && r_cb_data->show_memleaks) ||
+								(r_cb_data->show_clipped && stdout_key && xmlStrstr(stdout_key, (const xmlChar*)"clipped") && xmlStrstr(stdout_key, (const xmlChar*)" 0% samples clipped") == NULL) ) {
+			
+								if( r_cb_data->dokuwiki_format )
+								{
+									if( write_src_header ) {
+										fprintf(r_cb_data->report_fd, "===== %s =====\n", sourcefile_key);
+										/* disable so the header is only written once */
+										write_src_header = 0;
+									}
+									fprintf(r_cb_data->report_fd, "==== %s ====\n", name_key);
+									if( bios_key )
+										fprintf(r_cb_data->report_fd, "  * BIOS: ''%s''\n", bios_key);
+									if( ramsize_key )
+										fprintf(r_cb_data->report_fd, "  * Ramsize: ''%s''\n", ramsize_key);
+									if( devices_node )
+									{
+										xmlAttrPtr dev_attrs = devices_node->properties;
+										while( dev_attrs ) {
+											fprintf(r_cb_data->report_fd, "  * Device: ''%s %s''\n", dev_attrs->name, dev_attrs->children->content);
+			
+											dev_attrs = dev_attrs->next;
+										}
+									}
+									if( autosave_key )
+										fprintf(r_cb_data->report_fd, " (autosave)");
+									fprintf(r_cb_data->report_fd, "  * Error code: ''%s''\n", exitcode_key);
+		
+									if( stdout_key )
+										fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n", stdout_key);
+			
+									if( stderr_key )
+										fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n\n", stderr_key); // TODO: why two '\n'
 								}
-							}
-							if( autosave_key )
-								fprintf(r_cb_data->report_fd, " (autosave)");
-							fprintf(r_cb_data->report_fd, "  * Error code: ''%s''\n", exitcode_key);
-
-							if( stdout_key )
-								fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n", stdout_key);
-	
-							if( stderr_key )
-								fprintf(r_cb_data->report_fd, "<code>\n%s\n</code>\n\n", stderr_key); // TODO: why two '\n'
-						}
-						else
-						{
-							fprintf(r_cb_data->report_fd, "%s: %s", sourcefile_key, name_key);
-							if( bios_key )
-								fprintf(r_cb_data->report_fd, " (bios %s)", bios_key);
-							if( ramsize_key )
-								fprintf(r_cb_data->report_fd, " (ramsize %s)", ramsize_key);
-							if( devices_node )
-							{
-								xmlAttrPtr dev_attrs = devices_node->properties;
-								while( dev_attrs ) {
-									fprintf(r_cb_data->report_fd, " (%s %s)", dev_attrs->name, dev_attrs->children->content);
-	
-									dev_attrs = dev_attrs->next;
+								else
+								{
+									fprintf(r_cb_data->report_fd, "%s: %s", sourcefile_key, name_key);
+									if( bios_key )
+										fprintf(r_cb_data->report_fd, " (bios %s)", bios_key);
+									if( ramsize_key )
+										fprintf(r_cb_data->report_fd, " (ramsize %s)", ramsize_key);
+									if( devices_node )
+									{
+										xmlAttrPtr dev_attrs = devices_node->properties;
+										while( dev_attrs ) {
+											fprintf(r_cb_data->report_fd, " (%s %s)", dev_attrs->name, dev_attrs->children->content);
+			
+											dev_attrs = dev_attrs->next;
+										}
+									}
+									if( autosave_key )
+										fprintf(r_cb_data->report_fd, " (autosave)");
+									fprintf(r_cb_data->report_fd, " (%s)\n", exitcode_key);
+		
+									if( stdout_key )
+										fprintf(r_cb_data->report_fd, "%s\n", stdout_key);
+			
+									if( stderr_key )
+										fprintf(r_cb_data->report_fd, "%s\n", stderr_key);
 								}
+			
+								data_written = 1;
 							}
-							if( autosave_key )
-								fprintf(r_cb_data->report_fd, " (autosave)");
-							fprintf(r_cb_data->report_fd, " (%s)\n", exitcode_key);
-
-							if( stdout_key )
-								fprintf(r_cb_data->report_fd, "%s\n", stdout_key);
-	
-							if( stderr_key )
-								fprintf(r_cb_data->report_fd, "%s\n", stderr_key);
+							xmlFree(stdout_key);
+							stdout_key = NULL;
+		
+							xmlFree(stderr_key);
+							stderr_key = NULL;
+		
+							xmlFree(exitcode_key);
+							exitcode_key = NULL;
 						}
-	
-						data_written = 1;
+						else if( xmlStrcmp(output_childs->name, (const xmlChar*)"devices") == 0 )
+						{
+							devices_node = output_childs;
+						}
 					}
-					xmlFree(stdout_key);
-					stdout_key = NULL;
-
-					xmlFree(stderr_key);
-					stderr_key = NULL;
-
-					xmlFree(exitcode_key);
-					exitcode_key = NULL;
+					output_childs = output_childs->next;
 				}
-				else if( xmlStrcmp(output_childs->name, (const xmlChar*)"devices") == 0 )
-				{
-					devices_node = output_childs;
-				}
+			
+				xmlFree(bios_key);
+				bios_key = NULL;
+				xmlFree(sourcefile_key);
+				sourcefile_key = NULL;
+				xmlFree(name_key);
+				name_key = NULL;
+		
+				xmlFreeDoc(doc);
+				doc = NULL;
 			}
-			output_childs = output_childs->next;
 		}
-	
-		xmlFree(bios_key);
-		bios_key = NULL;
-		xmlFree(sourcefile_key);
-		sourcefile_key = NULL;
-		xmlFree(name_key);
-		name_key = NULL;
+		break;
+		
+		case 1:
+		{
+			char* entry_name = get_filename(filename);
+			char* tmp_path = NULL;
+			append_string(&tmp_path, r_cb_data->compare_folder);
+			append_string(&tmp_path, FILESLASH);
+			append_string(&tmp_path, entry_name);
+			if( access(tmp_path, F_OK) == 0 ) {
+				int write_set_data = 1;
 
-		xmlFreeDoc(doc);
-		doc = NULL;
+				xmlDocPtr doc1 = xmlReadFile(filename, NULL, 0);
+				xmlDocPtr doc2 = xmlReadFile(tmp_path, NULL, 0);
+
+				xmlXPathContextPtr xpathCtx1 = xmlXPathNewContext(doc1);
+				xmlXPathContextPtr xpathCtx2 = xmlXPathNewContext(doc2);
+
+				COMPARE_ATTRIBUTE("/output/result", "exitcode")
+				COMPARE_ATTRIBUTE("/output/result", "stderr")
+				COMPARE_ATTRIBUTE("/output/result/dir[@name='snap']//file", "png_crc")
+
+				if( xpathCtx2 )
+					xmlXPathFreeContext(xpathCtx2);
+				if( xpathCtx1 )
+					xmlXPathFreeContext(xpathCtx1);
+
+				xmlFreeDoc(doc2);
+				doc2 = NULL;
+				xmlFreeDoc(doc1);
+				doc1 = NULL;
+			}
+			else {
+				fprintf(r_cb_data->report_fd, "%s - missing\n", entry_name);
+			}
+			free(tmp_path);
+			tmp_path = NULL;
+			free(entry_name);
+			entry_name = NULL;
+		}
+		break;
 	}
 	
 	return data_written;
@@ -331,6 +434,8 @@ static void create_report()
 	r_cb_data.ignore_exitcode_4 = config_ignore_exitcode_4;
 	r_cb_data.show_memleaks = config_show_memleaks;
 	r_cb_data.show_clipped = config_show_clipped;
+	r_cb_data.report_type = config_report_type;
+	r_cb_data.compare_folder = config_compare_folder;
 
 	if( config_group_data ) {
 		struct group_data* gd = NULL;
