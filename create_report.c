748b49b693d513c495eef785f50605caf437210a
Oliver Stöneberg <firewave@users.noreply.github.com>
2011-04-22 09:28:35 +0000

    yet another change to summary

--- create_report.c
+++ create_report.c
@@ -549,7 +549,7 @@ static void create_report()
 		fprintf(report_fd, "  * %d executed\n", r_cb_data.summary.executed);
 		fprintf(report_fd, "  * %d crashes\n", r_cb_data.summary.crashed);
 		fprintf(report_fd, "  * %d errors\n", r_cb_data.summary.errors);
-		fprintf(report_fd, "  * %d missing roms\n", r_cb_data.summary.missing);
+		fprintf(report_fd, "  * %d with missing roms\n", r_cb_data.summary.missing);
 		fprintf(report_fd, "  * %d with memory leaks\n", r_cb_data.summary.memleaks);
 		fprintf(report_fd, "  * %d with clipping\n", r_cb_data.summary.clipped);
 		fprintf(report_fd, "  * %d with mandatory devices\n", r_cb_data.summary.mandatory);
