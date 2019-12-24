1d8fe18a195d84ef3fad80d7c31c1fc8b18641d5
firewave <firewave@users.noreply.github.com>
2019-12-24 15:12:35 +0100

    removed obsolete create_release.bat and kzip.exe

--- create_release.bat
+++
@@ -1,3 +0,0 @@
-set PATH=C:\mingw\mingw64-w32\bin
-make clean all
-kzip /y mame_regtest_release.zip CHANGELOG common.c common.h config.c config.h create_image_xml.c create_image_xml.exe create_report.c create_report.exe create_report.vcxproj create_report.xml create_report.vcxproj.filters makefile mame_regtest.c mame_regtest.exe mame_regtest.sln mame_regtest.vcxproj mame_regtest.xml mame_regtest.vcxproj.filters README TODOS
