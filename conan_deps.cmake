4a4d0a14f435eb86a6296c1588767cc8c4264e3c
firewave <firewave@users.noreply.github.com>
2021-01-21 21:07:38 +0100

    conan_deps.cmake: updated conan.cmake to 0.15

--- conan_deps.cmake
+++ conan_deps.cmake
@@ -3,7 +3,7 @@ set(ENV{CONAN_USER_HOME} "${CMAKE_BINARY_DIR}")
 
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.14/conan.cmake"
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
             "${CMAKE_BINARY_DIR}/conan.cmake")
 endif()
 
