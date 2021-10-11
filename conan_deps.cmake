a6714341fe2fee5578111e5ba6e352e2273b9690
firewave <firewave@users.noreply.github.com>
2021-10-11 18:44:42 +0200

    conan_deps.cmake: check expected hash and enabled TLS verification for cmake-conan download

--- conan_deps.cmake
+++ conan_deps.cmake
@@ -2,10 +2,12 @@
 set(CONAN_SYSTEM_INCLUDES ON)
 set(ENV{CONAN_USER_HOME} "${CMAKE_BINARY_DIR}")
 
+set(CONAN_CMAKE_HASH_EXPECTED "75c92be7d739ab69c3c9a1cd0bf4728cd08da143a18776eb43f8e2af16accace")
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
-            "${CMAKE_BINARY_DIR}/conan.cmake")
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake"
+            EXPECTED_HASH SHA256=${CONAN_CMAKE_HASH_EXPECTED}
+            TLS_VERIFY ON)
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
