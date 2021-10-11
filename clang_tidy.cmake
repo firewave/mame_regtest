a8427e7063836ddfccc5354ccafec62afa6a8e9e
firewave <firewave@users.noreply.github.com>
2021-10-11 18:31:31 +0200

    clang_tidy.cmake: added run-clang-tidy-12 and run-clang-tidy-13 as programs to look for

--- clang_tidy.cmake
+++ clang_tidy.cmake
@@ -1,4 +1,4 @@
-find_program(RUN_CLANG_TIDY NAMES run-clang-tidy run-clang-tidy-11 run-clang-tidy-10 run-clang-tidy-9 run-clang-tidy-8)
+find_program(RUN_CLANG_TIDY NAMES run-clang-tidy run-clang-tidy-13 run-clang-tidy-12 run-clang-tidy-11 run-clang-tidy-10 run-clang-tidy-9 run-clang-tidy-8)
 message(STATUS "RUN_CLANG_TIDY=${RUN_CLANG_TIDY}")
 if (RUN_CLANG_TIDY)
     add_custom_target(run-clang-tidy ${RUN_CLANG_TIDY} -q -p=${CMAKE_BINARY_DIR} -j ${NPROC_HALF})
