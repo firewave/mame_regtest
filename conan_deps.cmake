e33ce2d1da25b2f023ecf35d45923809414c6b4f
firewave <firewave@users.noreply.github.com>
2021-10-15 18:25:13 +0200

    conan_deps.cmake: added logging of all steps

--- conan_deps.cmake
+++ conan_deps.cmake
@@ -1,4 +1,3 @@
-# TODO: add logging of each step
 set(CONAN_SYSTEM_INCLUDES ON)
 set(ENV{CONAN_USER_HOME} "${CMAKE_BINARY_DIR}")
 
@@ -23,24 +22,28 @@ else()
     set(ENV{PATH} "${CONAN_VENV_BIN}:$ENV{PATH}")
 endif()
 
+message(STATUS "Setting up Python virtuelenv")
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -m venv ${CONAN_VENV_PATH}
         RESULT_VARIABLE PROC_RES)
 if (NOT PROC_RES EQUAL 0)
     message(FATAL_ERROR "venv creation failed - ${PROC_RES}")
 endif()
 
+message(STATUS "Setting up pip Python package")
 execute_process(COMMAND python -m pip install --upgrade pip
         RESULT_VARIABLE PROC_RES)
 if (NOT PROC_RES EQUAL 0)
     message(FATAL_ERROR "pip installation failed - ${PROC_RES}")
 endif()
 
+message(STATUS "Setting up setuptools Python package")
 execute_process(COMMAND pip install --upgrade setuptools
         RESULT_VARIABLE PROC_RES)
 if (NOT PROC_RES EQUAL 0)
     message(FATAL_ERROR "setuptools installation failed - ${PROC_RES}")
 endif()
 
+message(STATUS "Setting up conan Python package")
 execute_process(COMMAND pip install --upgrade conan
         RESULT_VARIABLE PROC_RES)
 if (NOT PROC_RES EQUAL 0)
@@ -50,6 +53,7 @@ endif()
 conan_check(VERSION 1.0.0 REQUIRED)
 
 # always remove the locks - we only use this internally and each configuration has its own folder
+message(STATUS "Removing existing conan locks")
 execute_process(COMMAND conan remove --locks
         RESULT_VARIABLE PROC_RES)
 if (NOT PROC_RES EQUAL 0)
@@ -58,11 +62,12 @@ endif()
 
 # workaround for "WARN: settings.yml is locally modified, can't be updated" false positive - only the default is ever created and it is never touched
 if (EXISTS "${CMAKE_BINARY_DIR}/.conan/settings.yml.new")
-    message(STATUS "Finish settings.yml migration")
+    message(STATUS "Finishing settings.yml migration")
     file(RENAME "${CMAKE_BINARY_DIR}/.conan/settings.yml.new" "${CMAKE_BINARY_DIR}/.conan/settings.yml")
 endif()
 
 # TODO: make the build type configurable - "missing" might be enough for normal development
+message(STATUS "Running conan")
 if (MINGW)
     configure_file(${CMAKE_SOURCE_DIR}/default_with_cmake ${CMAKE_BINARY_DIR}/.conan/profiles/default_with_cmake COPYONLY)
     conan_cmake_run(CONANFILE conanfile.txt
