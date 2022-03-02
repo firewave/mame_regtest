ea37bc7f2911d43b3287eef87a16e761b9257294
firewave <firewave@users.noreply.github.com>
2022-03-02 21:22:27 +0100

    conan_deps.cmake: updated conan.cmake to 0.17.0

--- conan_deps.cmake
+++ conan_deps.cmake
@@ -1,13 +1,13 @@
 set(CONAN_SYSTEM_INCLUDES ON)
 set(ENV{CONAN_USER_HOME} "${CMAKE_BINARY_DIR}")
 
-set(CONAN_CMAKE_HASH_EXPECTED "75c92be7d739ab69c3c9a1cd0bf4728cd08da143a18776eb43f8e2af16accace")
+set(CONAN_CMAKE_HASH_EXPECTED "3bef79da16c2e031dc429e1dac87a08b9226418b300ce004cc125a82687baeef")
 if(EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     file(SHA256 "${CMAKE_BINARY_DIR}/conan.cmake" CONAN_CMAKE_HASH_FOUND)
 endif()
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake" OR NOT "${CONAN_CMAKE_HASH_FOUND}" STREQUAL "${CONAN_CMAKE_HASH_EXPECTED}")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake"
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/0.17.0/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake"
             EXPECTED_HASH SHA256=${CONAN_CMAKE_HASH_EXPECTED}
             TLS_VERIFY ON)
 endif()
